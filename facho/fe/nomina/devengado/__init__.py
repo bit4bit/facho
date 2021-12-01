1454fe9abeeb92256b08042fb9a9a22087331090
bit4bit <bit4bit@noemail.net>
2021-12-01 00:28:56 +0000

    se adiciona DevengadoHorasExtrasNocturnas
    FossilOrigin-Name: b91f84e2a1e2973db222d88f3d29286aa43806404562bb9df81d512219439ca3

--- facho/fe/nomina/devengado/__init__.py
+++ facho/fe/nomina/devengado/__init__.py
@@ -8,6 +8,7 @@ __all__ = [
     'Devengado',
     'DevengadoBasico',
     'DevengadoTransporte',
+    'DevengadoHoraExtra',
     'DevengadoHorasExtrasDiarias',
-    'DevengadoHoraExtra'
+    'DevengadoHorasExtrasNocturnas'
 ]
