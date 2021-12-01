4944bb37ca0f8c2b18073fc432c6ab0d024ad5ca
bit4bit <bit4bit@riseup.net>
2021-12-01 00:34:29 +0000

    se adiciona DevengadoHorasRecargoDiariasDominicalesYFestivos
    FossilOrigin-Name: 75e778f2509c15cbd050799d101085b617564adf0e5ece50704112242d275b70

--- facho/fe/nomina/devengado/__init__.py
+++ facho/fe/nomina/devengado/__init__.py
@@ -12,5 +12,6 @@ __all__ = [
     'DevengadoHorasExtrasDiarias',
     'DevengadoHorasExtrasNocturnas',
     'DevengadoHorasRecargoNocturno',
-    'DevengadoHorasExtrasDiariasDominicalesYFestivos'
+    'DevengadoHorasExtrasDiariasDominicalesYFestivos',
+    'DevengadoHorasRecargoDiariasDominicalesYFestivos'
 ]
