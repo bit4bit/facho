788678f31a7e9f10ef068b7e2e629cedddcf2a72
bit4bit <bit4bit@noemail.net>
2021-12-06 17:39:37 +0000

    experimental: facho-signer: xml layout
    FossilOrigin-Name: 6fda6ceb8a3fd6d45cd68601b4608b9ce3a8f8af0eb79b99f9ec73a20a8023b3

---
+++ experimental/facho-signer/xades/minunit_ext.h
@@ -0,0 +1,13 @@
+#ifndef MINUNIT_EXT_H
+
+#include <string.h>
+
+#include "minunit.h"
+
+// cuando escribe esto el compilar me arrojo que si no era
+// mejor usar mu_assert_string_eq increble a 
+//int mu_assert_string_equals(const char *a, const char *b) {
+//  return mu_assert(strcmp(a, b) == 0, "string not equals");
+//}
+
+#endif //MINUNIT_EXT_H
--- experimental/facho-wasm/xmlsec-wasm/build_libxml2.sh
+++ experimental/facho-wasm/xmlsec-wasm/build_libxml2.sh
@@ -6,14 +6,16 @@ tar xf libxml2-${LIBXML2_VERSION}.tar.gz
 
 mv libxml2-${LIBXML2_VERSION} libxml2
 
-cd libxml2
+pushd libxml2
 
 wasiconfigure  ./configure --enable-static --without-http --without-ftp --without-modules --without-python --without-zlib --without-lzma --without-threads --host=x86_64
 
 wasimake make clean
 wasimake make -j4
 
-mkdir -p ../vendor/libxml2/lib
-mkdir -p ../vendor/libxml2/include
-cp -r include/libxml2 ../vendor/libxml2/include
-cp -r .libs/libxml2.a ../vendor/libxml2/lib
+popd
+
+mkdir -p vendor/libxml2/lib
+mkdir -p vendor/libxml2/include
+cp -r libxml2/include/libxml2 vendor/libxml2/include
+cp -r libxml2/.libs/libxml2.a vendor/libxml2/lib
