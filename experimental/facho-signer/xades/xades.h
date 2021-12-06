3fd1bfb0a7ef8b634f4f0782df763016fa6b46ea
bit4bit <bit4bit@noemail.net>
2021-12-06 22:46:30 +0000

    facho-xades: se adicionan mas etiquetas xml
    FossilOrigin-Name: 9891b60b972442eb5244d54d397498678cdd041fb7f7de44905f6b33a89e0849

--- experimental/facho-signer/xades/xades.h
+++ experimental/facho-signer/xades/xades.h
@@ -4,6 +4,7 @@
 #include <libxml/tree.h>
 
 #include <xmlsec/xmltree.h>
+#include <xmlsec/transforms.h>
 
 #include "xmlsec1/errors_helpers.h"
 
@@ -12,11 +13,33 @@ static const xmlChar xmlXadesNodeSignedProperties[] = "SignedProperties";
 
 static const xmlChar xmlXadesNodeSignedSignatureProperties[] = "SignedSignatureProperties";
 static const xmlChar xmlXadesNodeSigningTime[] = "SigningTime";
+static const xmlChar xmlXadesNodeSigningCertificate[] = "SigningCertificate";
+static const xmlChar xmlXadesNodeCertificate[] = "Cert";
+static const xmlChar xmlXadesNodeSignaturePolicyIdentifier[] = "SignaturePolicyIdentifier";
+static const xmlChar xmlXadesNodeSignaturePolicyId[] = "SignaturePolicyId";
+static const xmlChar xmlXadesNodeSigPolicyId[] = "SignaturePolicyId";
+static const xmlChar xmlXadesNodeIdentifier[] = "Identifier";
+static const xmlChar xmlXadesNodeDescription[] = "Description";
+static const xmlChar xmlXadesNodeSigPolicyHash[] = "SigPolicyHash";
+
+static const xmlChar xmlXadesNodeSignerRole[] = "SignerRole";
+static const xmlChar xmlXadesNodeClaimedRoles[] = "ClaimedRoles";
+static const xmlChar xmlXadesNodeClaimedRole[] = "ClaimedRole";
 
 static const xmlChar xmlXadesDSigNs[] = "http://uri.etsi.org/01903/v1.3.2#";
 
 xmlNodePtr
 xmlXadesTmplQualifyingPropertiesCreateNsPref(xmlDocPtr doc, const xmlChar* id, const xmlChar* nsPrefix);
+
+
 xmlNodePtr
 xmlXadesTmplAddSignedSignatureProperties(xmlNodePtr parentNode, const xmlChar* id, struct tm* signingTime);
+
+xmlNodePtr
+xmlXadesTmplAddSigningCertificate(xmlNodePtr parentNode);
+xmlNodePtr
+xmlXadesTmplAddCert(xmlNodePtr signingCertificateNode);
+xmlNodePtr
+xmlXadesTmplAddSignaturePolicyIdentifierSignaturePolicyId(xmlNodePtr signedSignaturePropertiesNode);
+
 #endif //XADES_H
