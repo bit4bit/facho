c128d3593430df88b570b4df512905b825e56799
bit4bit <bit4bit@riseup.net>
2021-12-26 20:21:04 +0000

    facho-signer: se adiciona cabezera copyleft
    FossilOrigin-Name: 689429ff0f376deb1cc4f5ac6b6d260bea421695eacb66fed71ec6567363e9b3

--- experimental/facho-signer/src/xades/templates.c
+++ experimental/facho-signer/src/xades/templates.c
@@ -1,3 +1,8 @@
+/**
+ * This file is part of facho.  The COPYRIGHT file at the top level of
+ * this repository contains the full copyright notices and license terms.
+ */
+
 #include "xades.h"
 
 #include <xmlsec/templates.h>
